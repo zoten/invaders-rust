fa930c2e81679f2ae217422b27dbde23c83eb0e6
Luca Dei Zotti <luca.deizotti@gmail.com>
2024-02-13 16:42:31 +0100

    [step 6] win and lose conditions

--- src/main.rs
+++ src/main.rs
@@ -103,6 +103,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw & render
         // player.draw(&mut curr_frame);
@@ -116,6 +119,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         // so the rx will not exist yet
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // Win or Lose?
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     // Cleanup
